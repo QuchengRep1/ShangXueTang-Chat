78924673e5d7869e1b57056a3f9c18e061055530
QuchengRep1 <QuchengRep_1@163.com>
2021-02-23 16:47:39 +0800

    7st in Office ,at 20210223 16:40.

--- src/main/java/ChatClient.java
+++ src/main/java/ChatClient.java
@@ -3,6 +3,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
@@ -10,6 +11,9 @@ import java.net.UnknownHostException;
 public class ChatClient extends Frame{
     TextField tfText = new TextField();
     TextArea taContent = new TextArea();
+    Socket s = null;
+    DataOutputStream dos = null;
+
     public static void main(String[] args) {
         ChatClient chatClient = new ChatClient();
         chatClient.launchFrame();
@@ -26,6 +30,7 @@ public class ChatClient extends Frame{
         addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
+                disconnect();
                 System.exit(0);
             }
         });
@@ -39,22 +44,40 @@ public class ChatClient extends Frame{
 
         @Override
         public void actionPerformed(ActionEvent e) {
-            String s = tfText.getText().trim();
-            taContent.setText(s);
+            String str = tfText.getText().trim();
+            taContent.setText(str);
             tfText.setText("");
+            try {
+                dos.writeUTF(str);
+                dos.flush();
+                //dos.close();
+
+
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+
         }
     }
 
     public void connect() {
         try {
-            Socket s = new Socket("127.0.0.1", 9989);
+            s = new Socket("127.0.0.1", 9989);
+            dos = new DataOutputStream(s.getOutputStream());
             System.out.println("connected!");
 
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
 
-
+    public void disconnect() {
+        try {
+            dos.close();
+            s.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 
 
--- src/main/java/ChatServer.java
+++ src/main/java/ChatServer.java
@@ -1,16 +1,29 @@
+import java.io.DataInputStream;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
 public class ChatServer {
 
+    static boolean started = false;
+
     public static void main(String[] args) {
 
         try {
             ServerSocket ss = new ServerSocket(9989);
-            while (true) {
+            started = true;
+            while (started) {
+                boolean bConnect = false;
                 Socket s = ss.accept();
+                bConnect = true;
                 System.out.println("a client connected!");
+                DataInputStream dis = new DataInputStream(s.getInputStream());
+                while (bConnect) {
+                    String str = dis.readUTF();
+                    System.out.println(str);
+                }
+                dis.close();
+
 
             }
         } catch (IOException e) {
