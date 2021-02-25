96d35162fa05aefadd86b53e397c7ba69cf1e4be
QuchengRep1 <QuchengRep_1@163.com>
2021-02-25 16:30:43 +0800

    13st in Office ,at 20210225 16:30. 备注：新增每个C端发送后同步到所有的C端。

--- src/main/java/ChatServer.java
+++ src/main/java/ChatServer.java
@@ -1,13 +1,19 @@
 import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.io.EOFException;
 import java.io.IOException;
 import java.net.BindException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 
 public class ChatServer {
     boolean started = false;
     ServerSocket ss = null;
+    List<Client> clients = new ArrayList<>();
+
 
     public static void main(String[] args) {
         new ChatServer().start();
@@ -30,6 +36,7 @@ public class ChatServer {
                 Socket s = ss.accept();
                 Client c = new Client(s);
                 new Thread(c).start();
+                clients.add(c);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -45,16 +52,27 @@ public class ChatServer {
     class Client implements Runnable {
         private Socket s;
         private DataInputStream dis = null;
+        private DataOutputStream dos = null;
         private boolean bConnect = false;
 
         public Client(Socket s) {
             this.s = s;
             try {
                 this.dis = new DataInputStream(s.getInputStream());
+                this.dos = new DataOutputStream(s.getOutputStream());
                 bConnect = true;
             } catch (IOException e) {
                 e.printStackTrace();
             }
+        }
+
+        public void send(String str) {
+            try {
+                dos.writeUTF(str);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
 
         @Override
@@ -64,6 +82,22 @@ public class ChatServer {
                 while (bConnect) {
                     String str = dis.readUTF();
                     System.out.println(str);
+                    for (int i=0;i<clients.size();i++) {
+                        Client c = clients.get(i);
+                        c.send(str);
+                    }
+
+/*                    for (Iterator<Client> it = clients.iterator(); it.hasNext();) {
+                        Client c = it.next();
+                        c.send(str);
+                   } //使用Iterator接口实现List的遍历
+*/
+/*                   Iterator<Client> it = clients.iterator();
+                   while (it.hasNext()) {
+                       Client c = it.next();
+                       c.send(str);
+                   }
+*/
                 }
             } catch (EOFException e) {
                 System.out.println("Client closed!");
@@ -72,14 +106,14 @@ public class ChatServer {
             } finally {
                 try {
                     if(dis != null) dis.close();
+                    if(dos != null) dos.close();
                     if(s  !=null) s.close();
                 }catch (IOException e1) {
                     e1.printStackTrace();
                 }
-
             }
-
-
         }
     }
 }
+
+
